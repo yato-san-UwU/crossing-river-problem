d661da69429db97412cd506589a6d95bdd74b054
rems-UwU <94802111+yato-san-UwU@users.noreply.github.com>
2023-01-07 20:38:35 +0100

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 
 
 int main(){
-    key_t key=ftok("/mnt/c/Users/yato/Documents/TP M1/systeme/projet",'c'); // mettre le chemin de linux
+    key_t key=ftok("chemin ou ce trouve votre fichier",'c'); // mettre le chemin de linux
 
 
     int sem=semcreate(key,5); // possible que 4 je sais pas aa voir
@@ -21,16 +21,17 @@ int main(){
     sem_init(sem,1,0); // hackerQ = 0
     sem_init(sem,2,0); // windowsQ = 0
     sem_init(sem,3,0); // compteur
-    // voir si doit rajouter autres choses
+    // initialisation des semaphore
     printf("l'id du groupe de semaphores = '%d'\n",sem);
 
-    key_t key2=ftok("/mnt/c/Users/yato/Documents/TP M1/systeme/projet",3); // mettre le chemin de linux
+    key_t key2=ftok("chemin ou ce trouve votre fichier",3); // mettre le chemin de linux
 
     int shmid=shmget(key,sizeof(sdata),IPC_CREAT|0666); //key 2
     printf("l'id du segment memoire shmid = '%d'\n",shmid);
     if(shmid==-1){
         perror("shmget");
     }
+    //initialisation des variable partager
     sdata *sd=NULL;
     sd=shmat(shmid,sd,0);
     sd->hackers=0;
